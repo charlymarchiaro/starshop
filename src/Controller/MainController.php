9d099f9dee899890be0d5fed571e949ad796db8c
Charly Marchiaro <carlosmarchiaro@gmail.com>
2025-01-29 10:20:07 -0300

    Chapter 14

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,30 +6,21 @@ use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
     public function homepage(
-        StarshipRepository  $starshipRepository,
-        HttpClientInterface $client,
-        CacheInterface      $issLocationPool,
+        StarshipRepository $starshipRepository,
     ): Response
     {
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
 
-        $issData = $issLocationPool->get('iss_location_data', function () use ($client) {
-            $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
-            return $response->toArray();
-        });
 
         return $this->render('main/homepage.html.twig', [
             'myShip' => $myShip,
             'ships' => $ships,
-            'issData' => $issData,
         ]);
     }
 }
