0ce192e5c460e58841d2660657e98a2f6c4c1a6b
Charly Marchiaro <carlosmarchiaro@gmail.com>
2025-01-28 22:09:27 -0300

    Chapter 11

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MainController extends AbstractController
